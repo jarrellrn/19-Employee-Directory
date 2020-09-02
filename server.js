58fd2f7725ce48ac6a9db59a39b5189b26b1e981
jarrellrn <jarrellrn@gmail.com>
2020-09-02 10:36:32 -0500

    updated server.js

--- server.js
+++ server.js
@@ -2,15 +2,14 @@ const path = require("path");
 const express = require("express");
 const app = express();
 
-const publicPath = path.join(__dirname, "public");
+app.use(express.static(path.join(__dirname, "build")));
+
 const port = process.env.PORT || 3000;
 
-app.use(express.static(publicPath));
-
 app.get("*", (req, res) => {
-  res.sendFile(path.join(publicPath, "index.html"));
+  res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
 app.listen(port, () => {
-  console.log("Server is up!");
+  console.log(`Server listening on http://localhost: ${port}`);
 });
